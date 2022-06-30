810a1b845067c0806826e9f9627d99bd2ec86973
Darius Garcia <dariusgarcia888@gmail.com>
2022-06-30 15:31:38 -0700

    media queries for schedule component

--- src/components/Schedule/Schedule.js
+++ src/components/Schedule/Schedule.js
@@ -1,3 +1,6 @@
+import honeyDip from "../../images/flyer/HoneyDip.jpeg";
+import honeyDip2 from "../../images/flyer/HoneyDip2.jpeg";
+import merrow from "../../images/flyer/TheMerrow.jpeg";
 import "./Schedule.scss";
 import * as Scroll from "react-scroll";
 
@@ -9,35 +12,101 @@ function Schedule() {
       <div className="schedule-container">
         <header className="schedule-header">
           <h1 id="schedule">Schedule</h1>
+          <button type="button" className="btn-book-shows">
+            Click to Book
+          </button>
         </header>
+
         <section className="schedule-list">
           <article className="shows-list-article">
-            <h3>Saturday, July 7, 2019</h3>
-            <h2>HoneyDip Riddim in La Jolla, California</h2>
+            <div className="schedule-event-dates">
+              <h3>Saturday, February 26, 2022</h3>
+              <button type="button" className="btn-buy-tickets">
+                Buy Tickets
+              </button>
+            </div>
+            <div className="schedule-events">
+              <img src={merrow} alt="merrow" className="flyers" />
+              <h2>The Merrow in San Diego, California</h2>
+            </div>
           </article>
+
           <article className="shows-list-article">
-            <h3>Saturday, July 7, 2019</h3>
-            <h2>HoneyDip Riddim in La Jolla, California</h2>
+            <div className="schedule-event-dates">
+              <h3>Saturday, February 26, 2022</h3>
+              <button type="button" className="btn-buy-tickets">
+                Buy Tickets
+              </button>
+            </div>
+            <div className="schedule-events">
+              <img src={honeyDip} alt="honeyDip" className="flyers" />
+              <h2>HoneyDip Riddim in La Jolla, California</h2>
+            </div>
           </article>
+
           <article className="shows-list-article">
-            <h3>Saturday, July 7, 2019</h3>
-            <h2>HoneyDip Riddim in La Jolla, California</h2>
+            <div className="schedule-event-dates">
+              <h3>Saturday, February 26, 2022</h3>
+              <button type="button" className="btn-buy-tickets">
+                Buy Tickets
+              </button>
+            </div>
+            <div className="schedule-events">
+              <img src={honeyDip2} alt="honeyDip2" className="flyers" />
+              <h2>HoneyDip Riddim in La Jolla, California</h2>
+            </div>
           </article>
+
           <article className="shows-list-article">
-            <h3>Saturday, July 7, 2019</h3>
-            <h2>HoneyDip Riddim in La Jolla, California</h2>
+            <div className="schedule-event-dates">
+              <h3>Saturday, February 26, 2022</h3>
+              <button type="button" className="btn-buy-tickets">
+                Buy Tickets
+              </button>
+            </div>
+            <div className="schedule-events">
+              <img src={honeyDip} alt="honeyDip" className="flyers" />
+              <h2>HoneyDip Riddim in La Jolla, California</h2>
+            </div>
           </article>
+
           <article className="shows-list-article">
-            <h3>Saturday, July 7, 2019</h3>
-            <h2>HoneyDip Riddim in La Jolla, California</h2>
+            <div className="schedule-event-dates">
+              <h3>Saturday, February 26, 2022</h3>
+              <button type="button" className="btn-buy-tickets">
+                Buy Tickets
+              </button>
+            </div>
+            <div className="schedule-events">
+              <img src={honeyDip} alt="honeyDip" className="flyers" />
+              <h2>HoneyDip Riddim in La Jolla, California</h2>
+            </div>
           </article>
+
           <article className="shows-list-article">
-            <h3>Saturday, July 7, 2019</h3>
-            <h2>HoneyDip Riddim in La Jolla, California</h2>
+            <div className="schedule-event-dates">
+              <h3>Saturday, February 26, 2022</h3>
+              <button type="button" className="btn-buy-tickets">
+                Buy Tickets
+              </button>
+            </div>
+            <div className="schedule-events">
+              <img src={honeyDip} alt="honeyDip" className="flyers" />
+              <h2>HoneyDip Riddim in La Jolla, California</h2>
+            </div>
           </article>
+
           <article className="shows-list-article">
-            <h3>Saturday, July 7, 2019</h3>
-            <h2>HoneyDip Riddim in La Jolla, California</h2>
+            <div className="schedule-event-dates">
+              <h3>Saturday, February 26, 2022</h3>
+              <button type="button" className="btn-buy-tickets">
+                Buy Tickets
+              </button>
+            </div>
+            <div className="schedule-events">
+              <img src={honeyDip} alt="honeyDip" className="flyers" />
+              <h2>HoneyDip Riddim in La Jolla, California</h2>
+            </div>
           </article>
         </section>
       </div>
